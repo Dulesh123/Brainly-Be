7867f14c32a0f5d6652fa6a2f3cbe40f93bb9751
Dulesh Shivakale <120615151+Dulesh123@users.noreply.github.com>
2026-06-29 22:24:04 +0530

    chaning the email sending format to smtp

--- routes/Forgotpassword.js
+++ routes/Forgotpassword.js
@@ -10,6 +10,7 @@ const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
   port: 465,
   secure: true,
+  family: 4, // 👈 force IPv4
   auth: {
     user: process.env.EMAIL,
     pass: process.env.APP_PASSWORD,
